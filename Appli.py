b289b5f8a146d26118ca4d51d9cade97d12cf508
kemuelos <72893403+kemuelos@users.noreply.github.com>
2022-01-12 10:46:07 -0400

    Add files via upload

--- Appli.py
+++ Appli.py
@@ -7,19 +7,19 @@ window = Tk()
 window.title("Qr Generateur")
 window.geometry("1080x720")
 window.minsize(480, 360)
-window.config(background="#FFFFFF")
+window.config(background="#1BB221")
 window.iconbitmap("ca.ico")
 
 
 #creer frame (boite)
-frame_1 = Frame(window, background="#FFFFFF")
-frame_2 = Frame(window,background="#FFFFFF")
+frame_1 = Frame(window, background="#1BB221")
+frame_2 = Frame(window,background="#1BB221")
 
 #creer les widgets (contenus dans la frame 1)
-label_title = Label(frame_1, text="Générateur de QR Code du crédit agricole", font=("Arial", 27), background='#FFFFFF', fg='black')
+label_title = Label(frame_1, text="Générateur de QR Code du crédit agricole", font=("Arial", 27), background='#1BB221', fg='white')
 label_title.pack()
 
-label_subtitle = Label(frame_1, text="Bienvenue sur le générateur de Qr Code, choisissez une option : ", font=("Arial", 15), bg='#FFFFFF', fg='black')
+label_subtitle = Label(frame_1, text="Bienvenue sur le générateur de Qr Code, choisissez une option : ", font=("Arial", 15), bg='#1BB221', fg='white')
 label_subtitle.pack()
 
 #affichage de la frame

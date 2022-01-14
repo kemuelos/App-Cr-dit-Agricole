cfe8ae0e766eb9a1f19a07f6573932ad9e5b6421
kemuelos <72893403+kemuelos@users.noreply.github.com>
2022-01-14 13:19:27 -0400

    Add files via upload

--- Appli.py
+++ Appli.py
@@ -2,13 +2,13 @@ from cProfile import label
 import tkinter as tk 
 from tkinter import * 
 from tkinter import font
+from tkinter.tix import COLUMN
+from turtle import left, right
 from webbrowser import get 
 import qrcode
 from PIL import Image
 
     
-    
-
 
 #fonction QR évenementiel
 def QR_code():
@@ -32,16 +32,19 @@ def QR_code():
     QRcode.make()
 
 
-    QRimg = QRcode.make_image(fill_color='Green', back_color="white").convert('RGB')
+    QRimg = QRcode.make_image(fill_color='#006C50', back_color="white").convert('RGB')
 
     pos = ((QRimg.size[0] - logo.size[0]) // 2,
            (QRimg.size[1] - logo.size[1]) // 2)
     QRimg.paste(logo, pos)
 
     QRimg.save('code.jpeg')
+    champ_lien.delete(0,"end")    
+    
 
 
-
+def supp():
+    champ_lien.delete(0, END)
 
 #fonction pour passer à la page suivante
 def page_2():
@@ -70,17 +73,21 @@ def page_3to1():
 #crer fenetre
 window = Tk()
 window.title("Qr Generateur")
-window.geometry("1080x720")
+window.geometry("1200x800")
 window.minsize(480, 360)
-window.config(background="#1BB221")
+window.config(background="#FFFFFF")
 window.iconbitmap("ca.ico")
 
+#banderole 
+photo = PhotoImage(file='ca_banderole.png')
+label = Label(window, image=photo)    
+label.pack()
  
 
 #creer frame (boite)
-frame_1 = Frame(window, background="#1BB221")
-frame_2 = Frame(window,background="#1BB221")
-frame_3 = Frame(window,background="#1BB221")
+frame_1 = Frame(window, background="#FFFFFF")
+frame_2 = Frame(window,background="#FFFFFF")
+frame_3 = Frame(window,background="#FFFFFF")
 
 
 #affichage de la frame
@@ -89,54 +96,61 @@ frame_1.pack()
 
 
 #creer les widgets (contenus dans la frame 1)
-label_title = Label(frame_1, text="Générateur de QR Code du crédit agricole", font=("Arial", 27), background='#1BB221', fg='white')
+label_title = Label(frame_1, text="Générateur de QR Code", font=("Arial", 27), background='#FFFFFF', fg='black')
 label_title.pack()
 
-label_subtitle = Label(frame_1, text="Bienvenue sur le générateur de Qr Code, choisissez une option : ", font=("Arial", 15), bg='#1BB221', fg='white')
+label_subtitle = Label(frame_1, text="Bienvenue sur le générateur de Qr Code, choisissez une option : ", font=("Arial", 15), bg='#FFFFFF', fg='black')
 label_subtitle.pack()
 
 
 
 
 #boutons frame 1
-bouton_1 = Button(frame_1, text="Générer un QR Code de contact", font=("Arial"), bg ='#FFFFFF', fg='black', command=page_2)
+bouton_1 = Button(frame_1, text="Générer un QR Code de contact", font=("Arial"), bg ='#006C50', fg='white', command=page_2)
 bouton_1.pack()
 
-bouton_2 = Button(frame_1, text="Générer un QR Code événementiel", font=("Arial"), bg ='#FFFFFF', fg='black', command=page_3)
+bouton_2 = Button(frame_1, text="Générer un QR Code événementiel", font=("Arial"), bg ='#006C50', fg='white', command=page_3)
 bouton_2.pack()
 
-bouton_2 = Button(frame_1, text="Quitter", font=("Arial"), bg ='#FFFFFF', fg='black', command=window.quit)
+bouton_2 = Button(frame_1, text="Quitter", font=("Arial"), bg ='#ED1C24', fg='white', command=window.quit)
 bouton_2.pack()
 
 
 
 #boutons frame 2
-bouton_3 = Button(frame_2, text="Générer un Qr code pour un agent", font=("Arial"), bg ='#FFFFFF', fg='black')
-bouton_3.pack()
+txt_nom = Label(frame_2, text="Nom", font=("Arial"), bg="#FFFFFF", fg="black")
+txt_nom.place(x=1000, y=220)
+txt_nom.pack()
 
-bouton_4 = Button(frame_2, text="Générer un Qr code en sélectionnant une agence", font=("Arial"), bg ='#FFFFFF', fg='black')
-bouton_4.pack()
+# txt_prenom = Label(frame_2, text="Prénom", font=("Arial"), bg="#FFFFFF", fg="black")
+# txt_prenom.pack()
 
-bouton_5 = Button(frame_2, text="Retour", font=("Arial"), bg ='#FFFFFF', fg='black', command=page_2to1)
+# txt_agence = Label(frame_2, text="Agence", font=("Arial"), bg="#FFFFFF", fg="black")
+# txt_agence.pack()
+
+
+
+bouton_5 = Button(frame_2, text="Retour", font=("Arial"), bg ='#ED1C24', fg='white', command=page_2to1)
 bouton_5.pack()
 
 
 
 #boutons frame 3
-txt_lien = Label(frame_3, text="Entrez le lien  : ", font=("Arial"), bg ='#FFFFFF', fg='black')
-champ_lien = Entry(frame_3)    
+txt_lien = Label(frame_3, text="Entrez le lien  : ", font=("Arial"), bg ='#006C50', fg='black')
+champ_lien = Entry(frame_3)
 txt_lien.pack()
 champ_lien.pack()
 
 
 
 
-bouton_6 = Button(frame_3, text="Valider", font=("Arial"), bg ='#FFFFFF', fg='black', command= QR_code)
+bouton_6 = Button(frame_3, text="Valider", font=("Arial"), bg ='#006C50', fg='black', command= QR_code)
 bouton_6.pack()
 
 
 
-bouton_7 = Button(frame_3, text="Retour", font=("Arial"), bg ='#FFFFFF', fg='black', command=page_3to1)
+
+bouton_7 = Button(frame_3, text="Retour", font=("Arial"), bg ='#ED1C24', fg='black', command=page_3to1)
 bouton_7.pack()
 
 
